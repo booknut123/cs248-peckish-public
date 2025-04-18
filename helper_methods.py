3d26277120ea712852841a21a59adc02d2f38cdb
booknut123 <kailynmlau@gmail.com>
2025-04-17 21:10:08 -0400

    revert?

--- helper_methods.py
+++ helper_methods.py
@@ -235,27 +235,27 @@ def get_logIds_by_date(userId, date):
     return df
     
 
-# def get_dishes_from_meal_log(userID, date, meal):
-#     """
-#     Returns a database dishes (and their calories) associated with a given User ID for a given date
-#     """
-#     conn = connect_db()
-#     cur = conn.cursor()
+def get_dishes_from_meal_log(userID, date, meal):
+    """
+    Returns a database dishes (and their calories) associated with a given User ID for a given date
+    """
+    conn = connect_db()
+    cur = conn.cursor()
 
-#     logIDs = get_logIds_by_date(userID, date)
-#     meals = {}
-#     for id in logIDs:
-#         cur.exectue(f"SELECT dish_ids, dining_hall, meal_name WHERE (log_id = {id})")
+    logIDs = get_logIds_by_date(userID, date)
+    meals = {}
+    for id in logIDs:
+        cur.exectue(f"SELECT dish_ids, dining_hall, meal_name WHERE (log_id = {id})")
   
-#     for meal in meals:
-#         dish = meals[meal]
-#         for id in dish:
-#             cur.execute(f"SELECT * FROM dishes WHERE dish_id = {id}")
-#             d = cur.fetchall()[0]
-#             m.append({"meal": meal,"name": d[1], "calories": d[7]})
-#     df = pd.DataFrame(m)
-#     conn.close()
-#     return df
+    for meal in meals:
+        dish = meals[meal]
+        for id in dish:
+            cur.execute(f"SELECT * FROM dishes WHERE dish_id = {id}")
+            d = cur.fetchall()[0]
+            m.append({"meal": meal,"name": d[1], "calories": d[7]})
+    df = pd.DataFrame(m)
+    conn.close()
+    return df
 
 def create_meal(userID, dishID, hall, mealName, date):
     """
