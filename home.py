90e7b9dca693aaea65c0cfd6b354e90c93df82a4
Nina Howley <ninahowley04@gmail.com>
2025-04-23 18:45:38 -0400

    edited formatting in journal and favorites

--- home.py
+++ home.py
@@ -12,8 +12,6 @@ import helper_methods
 # This is needed to get the database
 # download_db_from_github()
 
-st.write("Test")
-
 DEBUG = False # keep False when testing Google Login
 #DEBUG = True # set to True, when you don't want to go through authentication
 
